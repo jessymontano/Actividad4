1b9d8c96ef0ef6a54e3e48af6247fd1deff0d5a9
jessy <a221205042@unison.mx>
2022-09-07 13:20:43 -0700

    Initial commit

---
+++ src/Actividad4.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Actividad4 {
+    static boolean esCurp(String curp){
+        Pattern pat = Pattern.compile("[A-Z][AEIOU][A-Z]{2}[0-9]{6}[HM][A-Z]{2}[A-Z[^AEIOU]]{3}[0-9|A-Z][0-9]");
+        Matcher mat = pat.matcher(curp);
+        return mat.matches();
+    }
+
+    public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("No se ingreso un argumento");
+        }else{
+            if(esCurp(args[0])) System.out.println(args[0] +" es un CURP");
+            else System.out.println(args[0] + " no es un CURP");
+        }
+    }
+}
